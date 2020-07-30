36f5aa44fc2fdb06e2ac6acae58b49b7a24f4983
fabian-nunes <fabiannunes12@gmail.com>
2020-07-30 20:21:20 +0100

    create user

--- src/main/java/com/estgoh/demo/services/UserService.java
+++ src/main/java/com/estgoh/demo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.estgoh.demo.services;
 
 import com.estgoh.demo.domain.User;
+import com.estgoh.demo.dto.UserDTO;
 import com.estgoh.demo.repository.UserRepository;
 import com.estgoh.demo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 }
