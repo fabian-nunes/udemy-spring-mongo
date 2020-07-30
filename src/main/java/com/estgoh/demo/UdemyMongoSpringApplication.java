b65598c4ab75eb31b1b2599be5fcc336e3b4d2c3
fabian-nunes <fabiannunes12@gmail.com>
2020-07-30 09:40:23 +0100

    First commit project created

---
+++ src/main/java/com/estgoh/demo/UdemyMongoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.estgoh.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyMongoSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemyMongoSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/estgoh/demo/UdemyMongoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estgoh.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyMongoSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
