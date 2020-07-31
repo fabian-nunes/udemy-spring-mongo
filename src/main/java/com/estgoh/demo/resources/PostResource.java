a14165d58f35980656223609415ddde6c4d64496
fabian-nunes <fabiannunes12@gmail.com>
2020-07-31 16:25:06 +0100

    Creating querys methods

--- src/main/java/com/estgoh/demo/resources/PostResource.java
+++ src/main/java/com/estgoh/demo/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.estgoh.demo.resources;
 import com.estgoh.demo.domain.Post;
 import com.estgoh.demo.domain.User;
 import com.estgoh.demo.dto.UserDTO;
+import com.estgoh.demo.resources.util.URL;
 import com.estgoh.demo.services.PostService;
 import com.estgoh.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,4 +28,11 @@ public class PostResource {
         return ResponseEntity.ok().body(obj);
     }
 
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+
+        return ResponseEntity.ok().body(list);
+    }
 }
